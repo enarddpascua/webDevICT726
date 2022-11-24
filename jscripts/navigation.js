066c8dd4a1fe3a8a14f2347e7d82486a4b123112
Enard <enard@Enards-MBP.modem>
2022-11-25 09:32:48 +1100

    finalised navigation component

--- jscripts/navigation.js
+++ jscripts/navigation.js
@@ -1,36 +1,52 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-    const element = document.getElementById("burger-icon-wrapper");
+function navFunction(el) {
+    el.classList.toggle("change");
+    const collapseElement = document.getElementById("burger-icon-wrapper");
     const navElement = document.getElementById("nav-parent");
     const linkHolder = document.getElementById("link-wrapper");
-    if(element.className === "change"){
-      navElement.style.width = "30%";
+
+    if(collapseElement.className === "change"){
+      navElement.style.width = "12%";
       for(i=0;i<5;i++){
-        let node = document.createElement("i");
-        let liNode = document.createElement("li");
-        let anchorNode = document.createElement("a");
-        let textNode;
         switch(i){
-          case 0:
-            textNode = document.createTextNode("Home");
-            break;
-          case 1:
-            textNode = document.createTextNode("Contact");
-            break;
-          case 2:
-            textNode = document.createTextNode("Designs");
-            break;
-          case 3:
-            textNode = document.createTextNode("About");
-            break;
-          default:
-            textNode = document.createTextNode("Portfolio");
+            case 0:
+               linkHolder.children[i].childNodes[0].textContent = "Home";
+               break;
+            case 1:
+                linkHolder.children[i].childNodes[0].textContent = "About";
+                break;
+            case 2:
+                linkHolder.children[i].childNodes[0].textContent = "Contact";
+                break;
+            case 3:
+                linkHolder.children[i].childNodes[0].textContent = "Designs";
+                break;
+            default:
+                linkHolder.children[i].childNodes[0].textContent = "Portfolio";
         }
-        anchorNode.appendChild(textNode);
-        liNode.appendChild(anchorNode);
-        linkHolder.appendChild(liNode);
       }
     }else{
-      navElement.style.width = "10%";
+      navElement.style.width = "4%";
+      let node;
+      for(i=0;i<5;i++){
+        linkHolder.children[i].childNodes[0].textContent = "";
+        node = document.createElement("i");
+        switch(i){
+            case 0:   
+               linkHolder.children[i].childNodes[0].appendChild(node).setAttribute("class","fa-solid fa-house");
+               break;
+            case 1:  
+                linkHolder.children[i].childNodes[0].appendChild(node).setAttribute("class","fa-solid fa-circle-info");
+                break;
+            case 2:  
+                linkHolder.children[i].childNodes[0].appendChild(node).setAttribute("class","fa-solid fa-address-book");
+                break;
+            case 3:  
+                linkHolder.children[i].childNodes[0].appendChild(node).setAttribute("class","fa-solid fa-paint-brush");
+                break;
+            default:
+                linkHolder.children[i].childNodes[0].appendChild(node).setAttribute("class","fa-solid fa-briefcase");
+
+        }
+      }
     }
   }
